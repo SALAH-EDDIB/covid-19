e8db9de7f35106ab82063cf555ee1a7031e66450
salah eddib <salaheddib.55@gmail.com>
2020-04-22 03:53:13 +0000

    end of  the first step

--- js/script.js
+++ js/script.js
@@ -1,16 +1,140 @@
+//*  :::::::::::::::::::::::       Selector 
+
+
 const testBtn = document.querySelector('.start-btn')
 const questionnaire = document.querySelector('.questionnaire')
 const Préambule = document.querySelector('.Préambule')
 const stepper = document.querySelectorAll('.stepper h1')
-console.log(stepper);
+const nextBtn = document.querySelector('.next')
+const previousBtn = document.querySelector('.previous')
+const currentquestion = document.querySelector('.question')
+const answerInputs = document.querySelector('.answer-inputs')
+const progressBar = document.querySelector('.bar')
+const questionNumber = document.querySelector('.question-number')
+
+// ?  :::::::::::::::::::      Event Listener
+
+testBtn.addEventListener('click', startTest)
 
 
 
-testBtn.addEventListener('click', () => {
+// ! :::::::::::::::::::     fuction 
+
+let currentQuestionIndex = 0
+
+
+function startTest() {
     stepper[0].classList.remove('select')
     stepper[1].classList.add('select')
     testBtn.style.display = 'none'
     Préambule.style.display = 'none'
     questionnaire.style.display = 'block'
 
+}
+
+nextBtn.addEventListener('click', () => {
+    currentQuestionIndex++
+    showQuestion(questions[currentQuestionIndex])
+    folowProgress(currentQuestionIndex)
+
 })
+
+previousBtn.addEventListener('click', () => {
+    currentQuestionIndex--
+    showQuestion(questions[currentQuestionIndex])
+    folowProgress(currentQuestionIndex)
+})
+
+
+function showQuestion(question) {
+
+    currentquestion.innerText = question.question
+    answerInputs.innerHTML = ''
+    const inputAnswer = question.input.answer
+    const input = question.input
+
+
+    if (question.input.type === 'radio') {
+
+        inputAnswer.forEach(answer => {
+
+            answerInputs.innerHTML += ` <div>
+                    <input type="radio" id="${answer.text}">
+                    <label for="${answer.text}">
+                        <i class="fas ${answer.icon}"></i>
+                        <span>${answer.text}</span> </label>
+                    </div>`
+
+
+        })
+
+
+
+    } else {
+
+        answerInputs.innerHTML += `<input type="number"  id="${input.name}" min="${input.min}" max="${input.max}" placeholder="34 - 42">
+                                    <span class="input-span">${input.name}</span>`
+
+
+    }
+
+
+}
+
+
+
+
+function folowProgress(number) {
+
+    const currentNmber = number + 1
+
+    questionNumber.innerText = currentNmber
+    progressBar.style.width = `calc(${currentNmber} * calc(100% / 22))`
+
+}
+
+
+
+
+
+
+
+
+
+
+const questions = [{
+    question: 'Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?',
+
+    input: {
+        type: 'radio',
+        answer: [{
+            text: 'Oui',
+            icon: 'fa-check'
+        }, {
+            text: 'Non',
+            icon: 'fa-times'
+        }]
+    }
+}, {
+    question: 'Quelle est votre température corporelle ?',
+
+    input: {
+        type: 'number',
+        name: 'degrés',
+        min: 34,
+        max: 42
+    }
+}, {
+    question: 'Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?',
+
+    input: {
+        type: 'radio',
+        answer: [{
+            text: 'Oui',
+            icon: 'fa-check'
+        }, {
+            text: 'Non',
+            icon: 'fa-times'
+        }]
+    }
+}]
